5117a2e25bab44b029f091a31b2ad7b731c3f28c
Gleb Otochkin <gleb.otochkin@gmail.com>
2022-08-13 12:44:04 +0000

    Added a sample poke rest api call

---
+++ rest_client/oci_test.go
@@ -0,0 +1,39 @@
+// Copyright 2022 Gleb Otochkin
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+/// Example of parsing different arguments including an array of values
+
+package main
+
+import (
+	//"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+func main(){
+	//
+	response, err := http.Get("http://pokeapi.co/api/v2/pokedex/kanto/")
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(responseData)
+}
